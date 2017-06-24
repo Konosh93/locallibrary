bc3e382fd3b2b066a564288f32ff19429930cd35
Omer <omer.rayah@gmail.com>
2017-06-24 14:19:46 -0700

    update ALLOWED_HOSTS new update

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 
-ALLOWED_HOSTS = ['locallibrary-headland-75566.herokuap.com','127.0.0.1']
+ALLOWED_HOSTS = ['young-headland-75566.herokuap.com','127.0.0.1']
 
 # Application definition
 
